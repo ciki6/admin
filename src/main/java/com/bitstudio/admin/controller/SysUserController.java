062209ac74612683d5d387c49dcf5e29974d5019
ciki6 <18694942447@163.com>
2019-07-01 17:27:37 +0800

    集成swagger

--- src/main/java/com/bitstudio/admin/controller/SysUserController.java
+++ src/main/java/com/bitstudio/admin/controller/SysUserController.java
@@ -1,6 +1,9 @@
 package com.bitstudio.admin.controller;
 
 import com.bitstudio.admin.service.SysUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Api("用户控制器")
 @RestController
 @RequestMapping("user")
 public class SysUserController {
@@ -15,11 +19,14 @@ public class SysUserController {
     @Resource
     private SysUserService sysUserService;
 
+    @ApiOperation(value = "获取用户信息", notes = "根据用户ID获取用户信息")
+    @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "Long")
     @GetMapping(value="/findByUserId")
     public Object findByUserId(@RequestParam Long userId) {
         return sysUserService.findByUserId(userId);
     }
 
+    @ApiOperation(value = "获取用户信息", notes = "获取所有用户列表")
     @GetMapping(value="/findAll")
     public Object findAll() {
         return sysUserService.findAll();
